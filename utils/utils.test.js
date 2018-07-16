021fa273b088ef942e66d05474de8a5d22a635b0
Jean Santos <jeansantosc@gmail.com>
2018-07-15 22:28:05 -0300

    using assertion library _expect_

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,19 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   let res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`);
-  }
+  expect(res)
+    .toBe(44)
+    .toBeA('number');
 });
 
 it('should square a number', () => {
   let res = utils.square(4);
 
-  if (res !== 16) {
-    throw new Error(`Expected 16, but got ${res}.`);
-  }
+  expect(res)
+    .toBe(16)
+    .toBeA('number');
 });
