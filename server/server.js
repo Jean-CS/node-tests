b17aa911c6c7c80881a291cbccb5e6595ad794fa
Jean Santos <jeansantosc@gmail.com>
2018-07-17 00:08:18 -0300

    using _expect_ with _supertest_

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ let app = express();
 
 app.get('/', (req, res) => {
   res.status(404).send({
-    error: 'Page not found.'
+    error: 'Page not found.',
+    name: 'WhatEver App 0.0.1'
   })
 });
 
