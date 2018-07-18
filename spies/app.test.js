0b9b9f363a226da4008fa4463c98bafc76b27da7
Jean Santos <jeansantosc@gmail.com>
2018-07-17 22:14:40 -0300

    test spy | toHaveBeenCalled()

--- spies/app.test.js
+++ spies/app.test.js
@@ -2,6 +2,8 @@ const expect = require('expect');
 
 describe('App', () => {
   it('should call the spy correctly', () => {
-    
+    let spy = expect.createSpy();
+    spy();
+    expect(spy).toHaveBeenCalled(); // true
   });
 });
