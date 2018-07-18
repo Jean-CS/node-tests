247dadb8722f26600d7074191da4f51534e7d029
Jean Santos <jeansantosc@gmail.com>
2018-07-17 22:11:55 -0300

    setup for testing spies

---
+++ spies/app.js
@@ -0,0 +1,7 @@
+let db = require('./db');
+
+module.exports.handleSignup = (email, password) => {
+  // check if email already exists
+  db.saveUser({ email,password });
+  // send welcome email
+};
