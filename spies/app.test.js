641ccbd781249cca4b66050006e20cf47485ea72
Jean Santos <jeansantosc@gmail.com>
2018-07-17 22:27:22 -0300

    using _rewire_ to test function calls and argument references

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,6 +1,22 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire("./app");
 
 describe('App', () => {
+  let db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__('db', db);
+
+  it('should call saveUser with user object', () => {
+    let email = "test@email.com";
+    let password = "awe123";
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
+  })
+
   it('should call the spy correctly', () => {
     let spy = expect.createSpy();
     spy('John', 65);
