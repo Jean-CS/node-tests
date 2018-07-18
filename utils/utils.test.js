19709b74f9eb551ad5b0e2174ffaff15e670b325
Jean Santos <jeansantosc@gmail.com>
2018-07-17 22:06:19 -0300

    using describe to organize tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,24 +2,46 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  let res = utils.add(33, 11);
+describe('Utils', () => {
+  // '#' common syntax to describe a specific method
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      let res = utils.add(33, 11);
 
-  expect(res)
-    .toBe(44)
-    .toBeA('number');
-});
+      expect(res)
+        .toBe(44)
+        .toBeA('number');
+    });
 
-it('should square a number', () => {
-  let res = utils.square(4);
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(3, 4, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
+  });
 
-  expect(res)
-    .toBe(16)
-    .toBeA('number');
+  it('should square a number', () => {
+    let res = utils.square(4);
+
+    expect(res)
+      .toBe(16)
+      .toBeA('number');
+  });
+
+  it('should async square two numbers', (done) => {
+    utils.asyncSquare(3, (sqr) => {
+      expect(sqr).toBe(9).toBeA('number');
+      done();
+    });
+  });
 });
 
 it('should set firstName and lastName', () => {
-  let user = {location: 'Taiwan', age: 35}; // javascript is pass by reference. this object will be updated when the method below call 'setName'
+  let user = {
+    location: 'Taiwan',
+    age: 35
+  }; // javascript is pass by reference. this object will be updated when the method below call 'setName'
   let res = utils.setName(user, 'Brittany Spears');
 
   expect(res)
@@ -30,29 +52,15 @@ it('should set firstName and lastName', () => {
     .toBeA('object');
 });
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(3, 4, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  });
-});
-
-it('should async square two numbers', (done) => {
-  utils.asyncSquare(3, (sqr) => {
-    expect(sqr).toBe(9).toBeA('number');
-    done();
-  });
-});
-
 // it('should expect some values', () => {
 //   // expect(12).toNotBe(5); // true
 
 //   // expect({name: 'John Doe'}).toEqual({name: 'John Doe'}); // true
 //   // expect({name: 'Brittany'}).toNotEqual({name: 'John Doe'}); // true
-  
+
 //   // expect([2, 3, 4]).toInclude(5); // false
 //   // expect([2, 3, 4]).toExclude(1); // true
-  
+
 //   expect({
 //     name: 'John',
 //     age: 24,
@@ -60,7 +68,7 @@ it('should async square two numbers', (done) => {
 //   }).toInclude({
 //     age: 24
 //   }); // true
-  
+
 //   expect({
 //     name: 'John',
 //     age: 24,
